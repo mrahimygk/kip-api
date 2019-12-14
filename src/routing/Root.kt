d3eaec5020928b451891149a96568a29c713550c
rahimy-m <mojtaba.rahimy@chmail.ir>
2019-12-14 16:55:45 +0330

    +root route

--- src/routing/Root.kt
+++ src/routing/Root.kt
@@ -1,32 +1,39 @@
 package routing
 
+import db.dao.KweetDaoImpl
+import db.dao.UserDaoImpl
 import io.ktor.application.call
-import io.ktor.html.respondHtml
-import io.ktor.response.respondText
+import io.ktor.locations.get
 import io.ktor.routing.Routing
-import io.ktor.routing.get
-import kotlinx.html.body
-import kotlinx.html.head
-import kotlinx.html.p
-import kotlinx.html.title
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+import routing.routes.Root
+import session.KweetSession
 
-
-fun Routing.root(){
-    get("/") {
-        call.respondText("Hi gogoli")
-    }
-
-    get("/html") {
-        call.respondHtml {
-            head {
-                title { +"Ktor juju" }
-            }
-
-            body {
-                p {
-                    +"hello"
-                }
-            }
+fun Routing.root(kweetDao: KweetDaoImpl, userDao: UserDaoImpl) {
+    get<Root> {
+        val user = call.sessions.get<KweetSession>()?.let {
+            userDao.getUser(it.userID)
         }
+
+        val top = kweetDao.top(10).map { kweetDao.getKweet(it) }
+        val latest = kweetDao.latest(10).map { kweetDao.getKweet(it) }
+
+//        call.respondText(user.toString())
+//        call.respond(FreeMarkerContent())
     }
+//
+//    get("/html") {
+//        call.respondHtml {
+//            head {
+//                title { +"Ktor juju" }
+//            }
+//
+//            body {
+//                p {
+//                    +"hello"
+//                }
+//            }
+//        }
+//    }
 }
