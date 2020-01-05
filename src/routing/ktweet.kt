17d89ccb945f2b400b90f0421c64b1f9d468bb6e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-01-05 21:56:50 +0330

    + get /ktweet/{id}

--- src/routing/ktweet.kt
+++ src/routing/ktweet.kt
@@ -46,8 +46,8 @@ fun Routing.ktweet(userDao: UserDao, kweetDao: KweetDao) {
         }
     }
 
-    post<NewKtweet>{
-        val user = call.sessions.get<KweetSession>()?.let{
+    post<NewKtweet> {
+        val user = call.sessions.get<KweetSession>()?.let {
             userDao.getUser(it.userID, null)
         }
 
@@ -56,10 +56,24 @@ fun Routing.ktweet(userDao: UserDao, kweetDao: KweetDao) {
         val code = post["code"] ?: return@post call.redirect(it)
         val text = post["text"] ?: return@post call.redirect(it)
 
-        if(user==null || !call.verifyCode(date, user, code)) call.redirect(Login())
+        if (user == null || !call.verifyCode(date, user, code)) call.redirect(Login())
         else {
             val id = kweetDao.createKweet(user.userID, text, null)
             call.redirect(ViewKtweet(id))
         }
+    }
+
+    get<ViewKtweet> {
+        val user = call.sessions.get<KweetSession>()?.let { userDao.getUser(it.userID, null) }
+        val date = System.currentTimeMillis()
+        val code = if (user != null) call.securityCode(date, user) else null
+
+        call.respond(
+            FreeMarkerContent(
+                "view_ktweet.ftl",
+                mapOf("user" to user, "ktweet" to kweetDao.getKweet(it.id), "date" to date, "code" to code),
+                user?.userID ?: ""
+            )
+        )
     }
 }
