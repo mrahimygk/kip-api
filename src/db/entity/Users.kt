9c493822ece86d2bafc360a9e1a18198d4e83f7f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-20 21:08:34 +0330

    removing id, + avatar

--- src/db/entity/Users.kt
+++ src/db/entity/Users.kt
@@ -3,8 +3,7 @@ package db.entity
 import org.jetbrains.exposed.sql.Table
 
 object Users: Table() {
-    val id = varchar("id", 20).primaryKey()
-    val email = varchar("email", 128).uniqueIndex()
-    val name = varchar("name", 256)
+    val email = varchar("email", 128).primaryKey()
+    val avatar = varchar("avatar", 256)
     val hash = varchar("hash", 64)
 }
