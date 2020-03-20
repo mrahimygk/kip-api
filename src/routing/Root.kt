6fd0c10411d0e3db4790b5233ff6180b318956f8
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-20 21:07:44 +0330

    c+a+l

--- src/routing/Root.kt
+++ src/routing/Root.kt
@@ -1,16 +1,11 @@
 package routing
 
-import application.redirect
 import db.dao.KweetDaoImpl
 import db.dao.UserDaoImpl
 import io.ktor.application.call
 import io.ktor.http.ContentType
-import io.ktor.http.Parameters
 import io.ktor.http.content.TextContent
 import io.ktor.locations.get
-import io.ktor.locations.post
-import io.ktor.request.receive
-import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import routing.routeutil.Root
