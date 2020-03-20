dd9a13425977406d0df82d046ab98ee10afdb4af
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-20 21:07:33 +0330

    -

--- src/routing/Root.kt
+++ src/routing/Root.kt
@@ -1,35 +1,22 @@
 package routing
 
+import application.redirect
 import db.dao.KweetDaoImpl
 import db.dao.UserDaoImpl
 import io.ktor.application.call
-import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.http.ContentType
+import io.ktor.http.Parameters
+import io.ktor.http.content.TextContent
 import io.ktor.locations.get
+import io.ktor.locations.post
+import io.ktor.request.receive
+import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
 import io.ktor.routing.Routing
-import io.ktor.sessions.get
-import io.ktor.sessions.sessions
 import routing.routeutil.Root
-import session.KweetSession
 
 fun Routing.root(kweetDao: KweetDaoImpl, userDao: UserDaoImpl) {
     get<Root> {
-        val user = call.sessions.get<KweetSession>()?.let {
-            userDao.getUser(it.userID)
-        }
-
-        val top = kweetDao.top(10).map { kweetDao.getKweet(it) }
-        val latest = kweetDao.latest(10).map { kweetDao.getKweet(it) }
-
-        val etagString = user?.userID + "," + top.joinToString { it.id.toString() } + latest.joinToString { it.id.toString() }
-        val etag = etagString.hashCode()
-
-        call.respond(
-            FreeMarkerContent(
-                "index.ftl",
-                mapOf("top" to top, "latest" to latest, "user" to user),
-                etag.toString()
-            )
-        )
+        call.respond(TextContent("""{"message":"hello world"}""", ContentType.Application.Json))
     }
 }
