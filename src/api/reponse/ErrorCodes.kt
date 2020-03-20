244c8721103b8adcc097d9652027992de8141274
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-20 22:11:27 +0330

    +error codes list

---
+++ src/api/reponse/ErrorCodes.kt
@@ -0,0 +1,10 @@
+package api.reponse
+
+object ErrorCodes {
+    val SUCCESS = Pair("Success", 200)
+    val EMPTY_PASSWORD = Pair("Empty password", 1001)
+    val EMPTY_EMAIL = Pair("Empty email", 1002)
+    val INVALID_PASSWORD = Pair("Invalid Password", 1003)
+    val INVALID_EMAIL = Pair("Invalid Email", 1004)
+    val USER_EXISTS = Pair("User exists / Email taken", 1005)
+}
