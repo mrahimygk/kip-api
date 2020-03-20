512b86eaba43ebe5675c7a3ef63daf8982559d04
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-20 21:09:27 +0330

    user dao gets  avatar also

--- src/db/dao/UserDaoImpl.kt
+++ src/db/dao/UserDaoImpl.kt
@@ -1,11 +1,11 @@
 package db.dao
 
-import org.jetbrains.exposed.sql.Database
-import pojo.User
-import java.io.File
 import db.entity.Users
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
+import pojo.User
+import java.io.File
 
 class UserDaoImpl(val db: Database = Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")) : UserDao {
     constructor(dir: File) : this(
@@ -21,27 +21,19 @@ class UserDaoImpl(val db: Database = Database.connect("jdbc:h2:mem:test", driver
         }
     }
 
-    override fun getUser(userId: String, hash: String?) = db.transaction {
+    override fun getUser(email: String, hash: String?) = db.transaction {
         Users.select {
-            Users.id.eq(userId)
+            Users.email.eq(email)
         }.mapNotNull {
             if (hash == null || it[Users.hash] == hash)
-                User(userId, it[Users.email], it[Users.name], it[Users.hash])
+                User(it[Users.email], it[Users.avatar], it[Users.hash])
             else null
         }.singleOrNull()
     }
 
-    override fun getUser(email: String) = db.transaction {
-        Users.select { Users.email.eq(email) }
-            .map {
-                User(it[Users.id], email, it[Users.name], it[Users.hash])
-            }.singleOrNull()
-    }
-
     override fun createUser(user: User) = db.transaction {
         Users.insert {
-            it[id] = user.userID
-            it[name] = user.name
+            it[avatar] = user.avatar
             it[email] = user.email
             it[hash] = user.hash
         }
