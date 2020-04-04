ebbee13bd489ebc484dc7f35d446006e9885797c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 19:34:00 +0430

    +batch insert

--- src/db/dao/DrawingDaoImpl.kt
+++ src/db/dao/DrawingDaoImpl.kt
@@ -1,7 +1,8 @@
 package db.dao
 
+import db.entity.DrawingModel
 import org.jetbrains.exposed.sql.Database
-import pojo.DrawingModel
+import org.jetbrains.exposed.sql.batchInsert
 
 class DrawingDaoImpl(
     private val db: Database = Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
@@ -11,20 +12,28 @@ class DrawingDaoImpl(
         TODO("Not yet implemented")
     }
 
-    override fun getAll(): List<DrawingModel> {
+    override fun getAll(): List<pojo.DrawingModel> {
         TODO("Not yet implemented")
     }
 
-    override fun get(noteId: String): List<DrawingModel> {
+    override fun get(noteId: String): List<pojo.DrawingModel> {
         TODO("Not yet implemented")
     }
 
-    override fun insert(drawingModel: DrawingModel, noteId: String) {
+    override fun insert(drawingModel: pojo.DrawingModel, noteId: String) {
         TODO("Not yet implemented")
     }
 
-    override fun batchInsert(drawingModel: List<DrawingModel>, noteId: String) {
-        TODO("Not yet implemented")
+    override fun batchInsert(drawingList: List<pojo.DrawingModel>, noteId: String) {
+        db.transaction {
+            DrawingModel.batchInsert(drawingList) { drawing ->
+                this[DrawingModel.id] = drawing.id
+                this[DrawingModel.noteId] = noteId
+                this[DrawingModel.path] = drawing.path
+                this[DrawingModel.createdDate] = drawing.createdDate
+                this[DrawingModel.modifiedDate] = drawing.modifiedDate
+            }
+        }
     }
 
     override fun close() {
