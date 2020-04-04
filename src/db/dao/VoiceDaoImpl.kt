c23ac3cb311fb602d24f254d327057fdf0da7428
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 18:03:15 +0430

    +db

--- src/db/dao/VoiceDaoImpl.kt
+++ src/db/dao/VoiceDaoImpl.kt
@@ -1,8 +1,12 @@
 package db.dao
 
+import org.jetbrains.exposed.sql.Database
 import pojo.VoiceModel
 
-class VoiceDaoImpl : VoiceDao {
+class VoiceDaoImpl(
+    private val db: Database = Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+
+) : VoiceDao {
     override fun init() {
         TODO("Not yet implemented")
     }
