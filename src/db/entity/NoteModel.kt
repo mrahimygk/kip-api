c398e642bd3b3c615a248fcfa8ee80f36f4bc95d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 11:23:52 +0430

    +checkbox list, id as string  for using uuid

--- src/db/entity/NoteModel.kt
+++ src/db/entity/NoteModel.kt
@@ -3,10 +3,10 @@ package db.entity
 import org.jetbrains.exposed.sql.Table
 
 object NoteModel : Table() {
-    val id = integer("id").primaryKey().autoIncrement()
+    val id = varchar("id", 128).primaryKey()
     val userEmail = varchar("user_email", 128).index()
     val title = varchar("title", 1024)
-    val content = varchar("content", 1024)
+    val content = varchar("content", 2048)
     val color = varchar("color", 10)
 
     /**
@@ -24,6 +24,11 @@ object NoteModel : Table() {
      */
     //val labelList = varchar("labelList", 1024)
 
+    /**
+     * ref [CheckboxModel]
+     */
+    //val checkboxList = varchar("checkboxList", 1024)
+
     //    val replyTo = integer("reply_to").index().nullable()
 //    val directReplyTo = integer("direct_reply_to").index().nullable()
     val createdDate = datetime("created_date")
@@ -32,7 +37,7 @@ object NoteModel : Table() {
 
 object DrawingModel : Table() {
     val id = integer("id").primaryKey().autoIncrement()
-    val noteId = integer("note_id").index()
+    val noteId = varchar("note_id", 128).index()
     val path = varchar("path", 1024)
     val createdDate = datetime("created_date")
     val modifiedDate = datetime("modified_date")
@@ -40,7 +45,7 @@ object DrawingModel : Table() {
 
 object VoiceModel : Table() {
     val id = integer("id").primaryKey().autoIncrement()
-    val noteId = integer("note_id").index()
+    val noteId = varchar("note_id", 128).index()
     val path = varchar("path", 1024)
     val createdDate = datetime("created_date")
     val modifiedDate = datetime("modified_date")
@@ -49,7 +54,7 @@ object VoiceModel : Table() {
 object LabelJoinNoteModel : Table() {
     val id = integer("id").primaryKey().autoIncrement()
     val labelId = integer("label_id").index()
-    val noteId = integer("note_id").index()
+    val noteId = varchar("note_id", 128).index()
     val createdDate = datetime("created_date")
     val modifiedDate = datetime("modified_date")
 }
@@ -63,6 +68,7 @@ object LabelModel : Table() {
 
 object CheckboxModel : Table() {
     val id = integer("id").primaryKey().autoIncrement()
+    val noteId = varchar("note_id", 128).index()
     val text = varchar("text", 128)
     val indent = integer("indent")
     val checked = bool("checked")
