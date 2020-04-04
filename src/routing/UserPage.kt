2304ca4200148535508543b458c83f38d85bfbde
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 17:58:17 +0430

    -

--- src/routing/UserPage.kt
+++ src/routing/UserPage.kt
@@ -1,12 +1,12 @@
 package routing
 
-import db.dao.KweetDao
+import db.dao.NoteDao
 import db.dao.UserDao
 import io.ktor.locations.get
 import io.ktor.routing.Route
 import routing.routeutil.UserPage
 
-fun Route.userPage(userDao: UserDao, kweetDao: KweetDao) {
+fun Route.userPage(userDao: UserDao, noteDao: NoteDao) {
 
     get<UserPage> {
 
