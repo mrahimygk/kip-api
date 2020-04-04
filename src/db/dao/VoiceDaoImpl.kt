c4c616500898f5974240cff689da59fd5fb83d5b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 19:46:35 +0430

    batch inserting voices

--- src/db/dao/VoiceDaoImpl.kt
+++ src/db/dao/VoiceDaoImpl.kt
@@ -1,6 +1,8 @@
 package db.dao
 
+import db.entity.VoiceEntity
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.batchInsert
 import pojo.VoiceModel
 
 class VoiceDaoImpl(
@@ -23,8 +25,16 @@ class VoiceDaoImpl(
         TODO("Not yet implemented")
     }
 
-    override fun batchInsert(voiceModel: List<VoiceModel>, noteId: String) {
-        TODO("Not yet implemented")
+    override fun batchInsert(voiceList: List<VoiceModel>, noteId: String) {
+        db.transaction {
+            VoiceEntity.batchInsert(voiceList) { voice ->
+                this[VoiceEntity.id] = voice.id
+                this[VoiceEntity.noteId] = noteId
+                this[VoiceEntity.path] = voice.path
+                this[VoiceEntity.createdDate] = voice.createdDate
+                this[VoiceEntity.modifiedDate] = voice.modifiedDate
+            }
+        }
     }
 
     override fun close() {
