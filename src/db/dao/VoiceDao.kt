3f4c45ff82babdc13e3ff19b346397fccbacca3d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 19:46:00 +0430

    matching plural context

--- src/db/dao/VoiceDao.kt
+++ src/db/dao/VoiceDao.kt
@@ -1,6 +1,5 @@
 package db.dao
 
-import pojo.CheckboxModel
 import pojo.VoiceModel
 import java.io.Closeable
 
@@ -11,5 +10,5 @@ interface VoiceDao : Closeable {
     fun getAll(): List<VoiceModel>
     fun get(noteId: String): List<VoiceModel>
     fun insert(voiceModel: VoiceModel, noteId: String)
-    fun batchInsert(voiceModel: List<VoiceModel>, noteId: String)
+    fun batchInsert(voiceList: List<VoiceModel>, noteId: String)
 }
