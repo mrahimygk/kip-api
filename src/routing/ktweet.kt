9f934b57f274214319583a4417360337b84cc37c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-04 17:58:44 +0430

    -

--- src/routing/ktweet.kt
+++ src/routing/ktweet.kt
@@ -1,24 +1,14 @@
 package routing
 
-import application.redirect
-import db.dao.KweetDao
+import db.dao.NoteDao
 import db.dao.UserDao
-import io.ktor.application.call
-import io.ktor.freemarker.FreeMarkerContent
-import io.ktor.http.Parameters
 import io.ktor.locations.get
 import io.ktor.locations.post
-import io.ktor.request.receive
-import io.ktor.response.respond
 import io.ktor.routing.Routing
-import io.ktor.sessions.get
-import io.ktor.sessions.sessions
-import routing.routeutil.Login
 import routing.routeutil.NewKtweet
 import routing.routeutil.ViewKtweet
-import session.KweetSession
 
-fun Routing.ktweet(userDao: UserDao, kweetDao: KweetDao) {
+fun Routing.ktweet(userDao: UserDao, noteDao: NoteDao) {
 
     post<NewKtweet> {
 //        val post = call.receive<Parameters>()
