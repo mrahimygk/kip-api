c1d47a84b0067e39707345db0f9ad3fe561176c0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-03 12:11:01 +0430

    reducing var names

--- src/pojo/NoteModel.kt
+++ src/pojo/NoteModel.kt
@@ -1,19 +1,18 @@
 package pojo
 
-import org.joda.time.DateTime
 import java.io.Serializable
 
 data class NoteModel(
     val id: String,
-    val userEmail: String,
+    val email: String,
     val title: String,
     val content: String,
     val color: String,
-    val drawingList: List<DrawingModel>,
-    val voiceList: List<VoiceModel>,
-    val labelList: List<LabelModel>,
-    val checkboxList: List<CheckboxModel>,
-    val isPinned: Boolean,
-    val createdDate: DateTime,
-    val modifiedDate: DateTime
+    val drawings: List<DrawingModel>,
+    val voices: List<VoiceModel>,
+    val labels: List<LabelModel>,
+    val checkboxes: List<CheckboxModel>,
+    val pinned: Boolean,
+    val created: String,
+    val modified: String
 ) : Serializable
